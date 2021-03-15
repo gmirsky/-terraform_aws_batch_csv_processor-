46e7181cc2fafddf5c25df27f4588caa1c4f0760
Gregory Mirsky <gmirsky@gmail.com>
2021-03-15 13:07:21 -0400

    parameterized bid percentage

--- deploy/aws_batch_compute_environment.tf
+++ deploy/aws_batch_compute_environment.tf
@@ -3,15 +3,16 @@ resource "aws_batch_compute_environment" "compute" {
   compute_resources {
     instance_role = aws_iam_instance_profile.ecs_instance_role.arn
     instance_type = [
-      "m3",
-      "m4"
+      "m4",
+      "m5",
+      "m6g"
     ]
-    max_vcpus          = 16
-    min_vcpus          = 0
-    security_group_ids = var.security_group_ids
-    subnets = data.aws_subnet_ids.subnets.ids
+    max_vcpus           = 16
+    min_vcpus           = 0
+    security_group_ids  = var.security_group_ids
+    subnets             = data.aws_subnet_ids.subnets.ids
     type                = "SPOT"
-    bid_percentage      = 25
+    bid_percentage      = var.bid_percentage
     spot_iam_fleet_role = aws_iam_role.spot_fleet_role.arn
   }
   service_role = aws_iam_role.aws_batch_service_role.arn
@@ -19,4 +20,5 @@ resource "aws_batch_compute_environment" "compute" {
   depends_on = [
     aws_iam_role_policy_attachment.aws_batch_service_role
   ]
+  tags = module.aws_user_tags.tags
 }
