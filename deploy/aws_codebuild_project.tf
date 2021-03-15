c4ed38beb29f0622fff79bd99bca34ebd2522285
Gregory Mirsky <gmirsky@gmail.com>
2021-03-15 13:07:49 -0400

    updated parameters

--- deploy/aws_codebuild_project.tf
+++ deploy/aws_codebuild_project.tf
@@ -1,14 +1,22 @@
 resource "aws_codebuild_project" "build" {
-  name          = "aws_batch_csv_processor"
-  description   = "AWS Batch-based CSV processor"
-  build_timeout = "5"
-  service_role  = aws_iam_role.codebuild_role.arn
+  name           = "aws_batch_csv_processor"
+  description    = "AWS Batch-based CSV processor"
+  build_timeout  = "5"
+  queued_timeout = "5"
+  service_role   = aws_iam_role.codebuild_role.arn
   artifacts {
     type = "NO_ARTIFACTS"
   }
+  cache {
+    type = "LOCAL"
+    modes = [
+      "LOCAL_DOCKER_LAYER_CACHE",
+      "LOCAL_SOURCE_CACHE"
+    ]
+  }
   environment {
     compute_type    = "BUILD_GENERAL1_SMALL"
-    image           = "aws/codebuild/docker:1.12.1"
+    image           = "aws/codebuild/standard:5.0"
     type            = "LINUX_CONTAINER"
     privileged_mode = true
     environment_variable {
@@ -28,4 +36,5 @@ resource "aws_codebuild_project" "build" {
     type     = "CODECOMMIT"
     location = aws_codecommit_repository.code_repo.clone_url_http
   }
+  tags = module.aws_user_tags.tags
 }
